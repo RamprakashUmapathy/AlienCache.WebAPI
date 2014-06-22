a9d272ca9fd330403873e4720a9f853fce045bbd
Justin Yoo <justin.yoo@aliencube.com>
2014-06-22 15:40:12 +1000

    Added AlienCache.WebApi.Tests project.

---
+++ SourceCodes/AlienCache.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.AlienCache.WebApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b25753dd-fe19-42c1-bf7c-98d7d96c666c")]
